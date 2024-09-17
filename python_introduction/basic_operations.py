db952d0fc49128ef789e3ac7e74ef0d0756ca888
Abasiodiong Udofia <enalsy22@gmail.com>
2024-09-17 09:47:51 +0100

    task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1, number2 = 10, 5
+
+print(f"Addition of {number1} and {number2} is", int(number1) + int(number2))
+print(f"Subtraction of {number1} and {number2} is", int(number1) - int(number2))
+print(f"Multiplication of {number1} and {number2} is", int(number1) * int(number2))
