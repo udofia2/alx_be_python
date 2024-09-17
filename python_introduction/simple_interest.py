2f230cc7ef8ec2b0d62314297114e2d4190df2fe
Abasiodiong Udofia <enalsy22@gmail.com>
2024-09-17 10:08:35 +0100

    simple interest updated

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,9 @@
-principals = 1000
-rates = 0.05
+principal = 1000
+rate = 0.05
 time = 3
 
+principals = principal
+rates = rate
 interests = int(principals) * float(rates) * int(time)
 
-print(f'The simple interest for principal = {principals}, rate = {rates}, time = {time} is:', interests )
+print(f'The simple interest is:', interests )
