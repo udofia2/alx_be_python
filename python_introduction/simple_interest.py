d9494ac42470475a66a253428badd853a83595f0
Abasiodiong Udofia <enalsy22@gmail.com>
2024-09-17 10:11:40 +0100

    simple interest updated

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,6 @@ rate = 0.05
 time = 3
 
 
-interests = principal * rate * time
+interest = principal * rate * time
 
-print(f'The simple interest is:', interests )
+print(f'The simple interest is:', interest )
