3dce138204ed7e4925f7c8f77d911ffc694e7112
Abasiodiong Udofia <enalsy22@gmail.com>
2024-09-26 08:27:42 +0100

    drawing patten

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,12 @@
-size =  int(input("Enter the size of the pattern:"))
+size = int(input("Enter the size of the pattern: "))
 
-i = 0
-while(i < size):
-  # for x in range(size):
-  print("*" * size, "\n")
-  i += 1
+while size <= 0:
+    print("Please enter a positive integer.")
+    size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
