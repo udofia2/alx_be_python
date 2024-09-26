26e5fc4c6c137ae71ca8a4b6d9b3e82bebde3fa0
Abasiodiong Udofia <enalsy22@gmail.com>
2024-09-26 06:33:11 +0100

    multiplier

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
-for x in range(1, 10):
+for x in range(1, 11):
   result = number * x
   print(f"{number} * {x} = {result}")
