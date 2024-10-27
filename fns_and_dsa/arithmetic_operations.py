a6dda43688b4c1903a0b33e416e61888142ba231
Abasiodiong Udofia <enalsy22@gmail.com>
2024-10-27 15:58:14 +0100

    arithmetic

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float or str:
+    """
+    Performs basic arithmetic operations.
+
+    Args:
+    num1 (float): The first number.
+    num2 (float): The second number.
+    operation (str): The arithmetic operation to perform. Accepts 'add', 'subtract', 'multiply', or 'divide'.
+
+    Returns:
+    float or str: The result of the arithmetic operation or an error message for division by zero.
+    """
+
+    
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        
+        return "Error: Invalid operation. Please choose 'add', 'subtract', 'multiply', or 'divide'."
