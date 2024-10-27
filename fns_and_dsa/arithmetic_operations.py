e1ab6e9b304a525f067114a19c285d6d8646babc
Abasiodiong Udofia <enalsy22@gmail.com>
2024-10-27 16:02:14 +0100

    arithmetic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float|str:
+def perform_operation(num1, num2, operation) -> float|str:
     """
     Performs basic arithmetic operations.
 
